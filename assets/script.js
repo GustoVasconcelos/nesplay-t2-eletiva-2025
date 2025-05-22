59e435b5de96fb4a51ad85687e52c44dad6de944
John-Roberto <milesjohnprower92@gmail.com>
2025-05-22 10:36:03 -0300

    Implementado teste de animação aos spans

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,9 @@ if (typeof anime !== "undefined") {
 } else {
     console.error("anime.js não foi carregado corretamente.");
 }
+
 window.onload = function () {
+    // Animação do #myDiv
     anime({
         targets: '#myDiv',
         translateX: 250,
@@ -12,4 +14,18 @@ window.onload = function () {
         duration: 2000,
         direction: 'alternate'
     });
+
+    // Animação dos <span>
+    anime({
+        targets: 'span',
+        translateY: [
+          { value: '-2.75rem', easing: 'easeOutExpo', duration: 600 },
+          { value: '0rem', easing: 'easeOutBounce', duration: 800, delay: 100 }
+        ],
+        rotate: ['-1turn', '0turn'],
+        delay: (_, i) => i * 50,
+        easing: 'easeInOutCirc',
+        loopDelay: 1000,
+        loop: true
+      });
 };
